e72701496e58d9e1a42f5d1b8151f9eb8afed7df
XuanLam <nxlam.512@gmail.com>
2022-03-31 05:46:32 +0700

    Initial commit two

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+
+const morgan = require('morgan')
+
 const app = express()
 const port = 3000
+
+app.use(morgan('combined'))
 
 app.get('/home', (req, res) => {
     return res.send('Hello world');
